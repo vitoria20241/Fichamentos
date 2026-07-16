a32df8360227446b3b47cde6ef8ff67910bbf183
vitoria20241 <165173745+vitoria20241@users.noreply.github.com>
2026-07-16 20:22:34 -0300

    Create pdf.py

---
+++ utils/pdf.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 16:07:30 2026
+
+@author: Vitoria
+"""
+
+import os
+
+PASTA_PDFS = "pdfs"
+
+def salvar_pdf(upload):
+    caminho = os.path.join(PASTA_PDFS, upload.name)
+
+    with open(caminho, "wb") as f:
+        f.write(upload.getbuffer())
+
+    return caminho 
+
+
+def excluir_pdf(caminho): 
+    if caminho and os.path.exists(caminho):
+        os.remove(caminho) 
