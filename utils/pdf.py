45756c31c6ae859c5499b7c554e10fe0b28e6e41
vitoria20241 <165173745+vitoria20241@users.noreply.github.com>
2026-08-14 13:44:20 -0300

    Update pdf.py

--- utils/pdf.py
+++ utils/pdf.py
@@ -6,11 +6,11 @@ Created on Thu Jul 16 16:07:30 2026
 """
 
 import os
+from config import PDFS_DIR
 
-PASTA_PDFS = "pdfs"
 
 def salvar_pdf(upload):
-    caminho = os.path.join(PASTA_PDFS, upload.name)
+    caminho = os.path.join(PDFS_DIR, upload.name)
 
     with open(caminho, "wb") as f:
         f.write(upload.getbuffer())
@@ -21,9 +21,3 @@ def salvar_pdf(upload):
 def excluir_pdf(caminho): 
     if caminho and os.path.exists(caminho):
         os.remove(caminho) 
-        
-
-def link_pdf(caminho_pdf):
-    pasta = "C:/Users/Vitoria/Documents/GitHub/Fichamentos/"
-    caminho_pdf = caminho_pdf.replace("\\", "/") 
-    return pasta + caminho_pdf 
